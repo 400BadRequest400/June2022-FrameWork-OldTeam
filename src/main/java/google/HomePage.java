2548009b9afbde390bd81c44cb192d4f756ad8a3
NotFound404 <djaf1981@gmail.com>
2022-06-08 16:35:48 -0400

    create a Google package

---
+++ src/main/java/google/HomePage.java
@@ -0,0 +1,47 @@
+package google;
+
+import base.CommonAPI;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage extends CommonAPI {
+
+    public HomePage(WebDriver driver){
+        PageFactory.initElements(driver, this);
+
+    }
+
+    @FindBy(xpath = "//body/div[1]/div[2]/div[1]/img[1]")
+    private WebElement logoSign;
+
+    @FindBy(xpath = "//a[contains(text(),'About')]")
+    private WebElement aboutGoogle;
+
+    @FindBy(xpath = "//a[contains(text(),'Store')]")
+    private WebElement googleStore;
+
+    @FindBy(xpath = "//div[contains(text(),'Shop by category.')]")
+    private WebElement shopByCategory;
+
+
+
+    public void clickOnGoogleLogoSign(){
+        click(logoSign);
+    }
+    public void clickOnGoogleAboutButton(){
+        click(aboutGoogle);
+    }
+    public void clickOnGoogleStore(){
+        click(googleStore);
+    }
+    public void getTextForShopByCategory(){
+        click(shopByCategory);
+        System.out.println(getElementText(shopByCategory));
+        String s = shopByCategory.getText();
+        System.out.println(s);
+
+    }
+
+}
